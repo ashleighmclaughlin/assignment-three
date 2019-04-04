6b0be4cf5e968908c7ce69b4e68084398f8a02b9
Tae Ji Lee <citruspandadesigns@gmail.com>
2019-04-04 21:05:21 +1300

    Added background slides and begun design component with header and footer

--- src/components/tj/Tj.vue
+++ src/components/tj/Tj.vue
@@ -1,38 +1,33 @@
 <template>
   <div>
-    <div class="hero">
+    <div class="demo">
       <header class="landing">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <router-link class="back" to="/">
-            <a>Back</a>
+            <i class="fas fa-chevron-left"></i>
           </router-link>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+          <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="navbar-toggler-icon"></i>
           </button>
   
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Our Designers</a>
+                <a class="nav-link text-white"><span><router-link v-bind:to="'/designers'">Our Designers</router-link></span></a>
               </li>
             </ul>
-
-            <form class="form-inline my-2 my-lg-0">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Our Designers</a>
-                </li>
-              </ul>
-            </form>
           </div>
         </nav>
       </header>
+      <transition-group tag="div" name="slide" class="img-slider">
+        <div v-for="number in [currentImg]" v-bind:key="number">
+          <img :src="imgList[Math.abs(currentImg) % imgList.length]" />
+        </div>
+      </transition-group>
+  
       <div class="hero-text">
         <h1>Nooble Creative</h1>
       </div>
@@ -44,21 +39,34 @@
 </template>
 
 <script>
+// import Designers from "./designers/Designers";
+
   export default {
-    name: "Tj"
-  };
+    name: "Tj",
+    data: function() {
+      return {
+        imgList: [
+          'https://imgur.com/p33fpEW.jpg',
+          'https://imgur.com/ZU5pfcV.jpg',
+          'https://imgur.com/97bvcV8.jpg',
+          'https://imgur.com/EpQwIod.jpg'
+          // https://imgur.com/a/qKrQK1G
+        ],
+        currentImg: 0
+      }
+    },
+    mounted() {
+      setInterval(() => {
+        this.currentImg = this.currentImg + 1;
+      }, 40000);
+    },
+    components: {
+      // Designers
+    }
+  }
 </script>
 
 <style scoped>
-  .hero {
-    background-image: url(../../assets/tj_img/studio-republic-644339-unsplash.jpg);
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    width: 100vw;
-    height: 100vh;
-  }
-  
   .hero-text {
     color: #ffffff;
     height: 100px;
@@ -68,8 +76,9 @@
     transform: translate(-50%, -50%);
   }
   
-  .nav-item .nav-link {
+  i {
     color: #ffffff;
+    font-size: 25px;
   }
   
   .logo {
@@ -77,16 +86,64 @@
     right: 30px;
     bottom: 20px;
   }
-
+  
   .logo-img {
-      height: 60px;
+    height: 60px;
   }
 
-  
-  
+ .hero-text h1 {
+   font-size: 70px;
+ }
+
+ .navbar-light .navbar-toggler {
+   color: #fff;
+   border-color: rgba(0, 0, 0, 0);
+ }
+
   /* Override bootstrap styles */
-  
   .navbar {
     background-color: transparent !important;
+    color: #ffffff;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    z-index: 2;
+  }
+
+  /*
+  * https://stackoverflow.com/questions/42866098/create-sliding-left-effect-using-vuejs-animation
+  */
+  .demo {
+    overflow: hidden;
+  }
+  
+  .slide-leave-active,
+  .slide-enter-active {
+    transition: 1s;
+  }
+  
+  .slide-enter {
+    transform: translate(100%, 0);
+  }
+  
+  .slide-leave-to {
+    transform: translate(-100%, 0);
+  }
+  
+  .img-slider {
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+    height: 100vh;
+  }
+  
+  .img-slider img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
   }
 </style>
---
+++ src/components/tj/designers/Designers.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <Header />
+        <p>Here are the designers!</p>
+      <footer />
+    </div>
+</template>
+
+<script>
+import Header from "./Header";
+import Header from "./Footer";
+
+export default {
+  name: "Designers",
+  data: function() {
+    return {
+      source: ""
+    };
+  },
+  components: {
+      Header,
+      Footer
+  },
+  methods: {
+    sourceChanged: function(source) {
+      this.source = source;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.router-link-active {
+  font-weight: 400;
+}
+</style>
