a565de754b42d90cf22f17d165c6a1fb42005507
Ashleigh Mclaughlin <932400796@dov2-sc-23.local>
2019-04-04 18:52:50 +1300

    Created landing page on my component, including navigation, footer, and header content

---
+++ src/components/ash/AshFooter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="footer">
+        <p>Nifty Studio Co  |   © Copyright 2019</p>
+    </div>
+</template> 
+
+<script>
+export default {
+  name: "AshFooter"
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Libre+Baskerville|Montserrat');
+.footer{
+    background-color: #feb954;
+    padding: 25px;
+    margin-top: 30px;
+}
+p{
+    font-family: 'Montserrat', sans-serif;
+    color: white;
+    margin: 0;
+}
+</style>
--- src/components/ash/Ashleigh.vue
+++ src/components/ash/Ashleigh.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
-      <p>Ash is the best</p>
+    <MyHeader />
+    <div class="home-title">
+      <h1>welcome to <span class="nifty-orange">nifty</span></h1>
+        <p class="text-center mx-auto"> 
+            We are an independent Design Studio based in Christchurch, New Zealand.
+            We showcase beautiful designers in the Canterbury region.
+        </p>
+    </div>
+    <AshFooter />
   </div>
 </template>
 
 <script>
+import MyHeader from "./MyHeader";
+import AshFooter from "./AshFooter";
+
 export default {
-  name: "Ashleigh"
+  name: "Ashleigh",
+  components: {
+      MyHeader,
+      AshFooter
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Libre+Baskerville|Montserrat');
+
+.nifty-orange{
+  color: #feb954;
+}
+
+.home-title h1{
+  font-family: 'Libre Baskerville', serif;
+  font-weight: bold;
+  font-size: 35px;
+  color: #222;
+  margin-top: 80px;
+}
+.home-title p{
+  width: 50%;
+  font-family: 'Montserrat', sans-serif;
+  margin-top: 20px;
+}
+</style>
