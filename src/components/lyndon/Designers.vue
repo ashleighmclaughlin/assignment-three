0482fc80354ad2e5efc88546279801d14dac8d2b
Web Work <webwork@Lyndons-iMac.local>
2019-04-05 14:56:53 +1300

    added the designers to the home page

--- src/components/lyndon/Designers.vue
+++ src/components/lyndon/Designers.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    
+
     <Header class="main-image-2" />
     <h1>Designers</h1>
-  
+    <ul>
+      <li
+        :key="designer"
+        v-for="designer in designers"
+        v-on:click="designer.show = !designer.show"
+      >
+        <h2>{{designer.name}}</h2>
+        <h3
+          class="speciality"
+          v-show="designer.show"
+        >{{designer.speciality}}</h3>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -12,23 +24,55 @@ export default {
   //   name: Designers
   components: {
     Header
+  },
+  data() {
+    return {
+      designers: [
+        { name: "Ryu", speciality: "Art work", show: false },
+        { name: "Crystal", speciality: "Pictures", show: false },
+        { name: "James", speciality: "Click Events", show: false }
+      ]
+    };
   }
 };
 </script>
 
 <style scoped>
 .main-image-2 {
- 
   background-image: url(layout/img/header-2.jpg);
   background-repeat: no-repeat, repeat;
-   background-size: cover;
-   
-} 
+  background-size: cover;
+}
 h1 {
   text-align: center;
   font-size: 3em;
 }
-
+#designers {
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+  background: rgb(223, 216, 174);
+  height: 350px;
+}
+.speciality {
+  color: coral;
+}
 </style>
 
 
--- src/components/lyndon/Lyndon.vue
+++ src/components/lyndon/Lyndon.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <Header class="main-image" />
-<div>
-    <h1>Our Designers</h1>
     <div>
-      <img src="layout/img/allef.jpg">
-    </div>
-    <h3>hehehe</h3>
+      <h1>Our Designers</h1>
+      <div class="designers">
+        <img
+          src="./layout/img/allef.jpg"
+          width="300"
+        >
+        <img
+          src="./layout/img/blake.jpg"
+          width="300"
+        >
+        <img
+          src="./layout/img/caleb.jpg"
+          width="300"
+        >
+      </div>
+
     </div>
   </div>
 </template>
@@ -24,15 +35,25 @@ export default {
 
 <style scoped>
 .main-image {
- 
   background-image: url(layout/img/header.jpg);
   background-repeat: no-repeat, repeat;
-   background-size: cover;
-  
-  
-} 
+  background-size: cover;
+}
 h1 {
   text-align: center;
   font-size: 3em;
+}
+.designers {
+  width: 1200px;
+  text-align: center;
+  padding-top: 20px;
+  margin: 0 auto;
+}
+img {
+  margin: 0 20px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
 }
 </style>
